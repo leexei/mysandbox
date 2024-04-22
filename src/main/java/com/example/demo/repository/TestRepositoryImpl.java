49d1022c868bf9da16700052e7c852fc4532c0ff
Tomoyuki Yamashita <tomoyuki.yamashita@zozo.com>
2024-04-22 17:32:58 +0900

    ベースのリポジトリに副作用が無いようにする

--- src/main/java/com/example/demo/repository/TestRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/TestRepositoryImpl.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@ConditionalOnProperty(name="using.mock", havingValue="false", matchIfMissing=true)
 public class TestRepositoryImpl implements TestRepository{
   public String generateWord() {
     return "Hello World!";
--- src/main/java/com/example/demo/repository/TestRepositoryMockImpl.java
+++ src/main/java/com/example/demo/repository/TestRepositoryMockImpl.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @ConditionalOnProperty(name="using.mock", havingValue="true", matchIfMissing=false)
+@Primary
 public class TestRepositoryMockImpl implements TestRepository{
   public String generateWord() {
     return "hehehe!";
